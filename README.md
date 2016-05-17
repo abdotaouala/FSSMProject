6ad1ac5ed42d42367b7aacce0c82870a402ea1f6
abdotaouala <abdotaouala@gmail.com>
2016-05-17 14:51:21 +0100

    taouala faire cette commit initial ;)

---
+++ README.md
@@ -0,0 +1,4 @@
+shiro-primefaces-example
+========================
+
+This example uses JSF (PrimeFaces) to login to Apache Shiro, on a PostgreSQL database.
---
+++ src/main/java/org/example/shiro/bean/security/ShiroLoginBean.java
@@ -0,0 +1,116 @@
+/**
+ * Very simple bean that authenticates the user via Apache Shiro, using JSF
+ * @author Daniel Mascarenhas
+ */
+package org.example.shiro.bean.security;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ejb.Stateless;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.logging.Level;
+import javax.faces.application.NavigationHandler;
+import org.apache.shiro.crypto.hash.Sha256Hash;
+
+@Named
+@Stateless
+@ViewScoped
+public class ShiroLoginBean implements Serializable {
+    private static final Logger log = LoggerFactory.getLogger(ShiroLoginBean.class);
+
+    private String username;
+    private String password;
+    private Boolean rememberMe;
+
+    public ShiroLoginBean() {
+    }
+
+    /**
+     * Try and authenticate the user
+     */
+    public void doLogin() {
+        Subject subject = SecurityUtils.getSubject();
+        System.out.println("***  password : " + new Sha256Hash(getPassword()).toHex());
+        UsernamePasswordToken token = new UsernamePasswordToken(getUsername(), new Sha256Hash(getPassword()).toHex(), getRememberMe());
+        
+        try {
+            subject.login(token);
+
+            if (subject.hasRole("admin")) {
+                FacesContext.getCurrentInstance().getExternalContext().redirect("admin/index.xhtml");
+            }
+            else {
+                FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
+            }
+        }
+        catch (UnknownAccountException ex) {
+            facesError("Login ou mot de passe incorrecte");
+            log.error(ex.getMessage(), ex);
+        }
+        catch (IncorrectCredentialsException ex) {
+            facesError("Mot de passe incorrecte");
+            log.error(ex.getMessage(), ex);
+        }
+        catch (LockedAccountException ex) {
+            facesError("Locked account");
+            log.error(ex.getMessage(), ex);
+        } catch (IOException ex) {
+            java.util.logging.Logger.getLogger(ShiroLoginBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        finally {
+            token.clear();
+        }
+    }
+
+    /**
+     * Adds a new SEVERITY_ERROR FacesMessage for the ui
+     * @param message Error Message
+     */
+    private void facesError(String message) {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, message, null));
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String login) {
+        this.username = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String senha) {
+        this.password = senha;
+    }
+
+    public Boolean getRememberMe() {
+        return rememberMe;
+    }
+
+    public void setRememberMe(Boolean lembrar) {
+        this.rememberMe = lembrar;
+    }
+    
+    public void authorizedUserControl(){
+
+        if(null != SecurityUtils.getSubject().getPrincipal()){
+
+            NavigationHandler nh =  FacesContext.getCurrentInstance().getApplication().getNavigationHandler();
+            nh.handleNavigation(FacesContext.getCurrentInstance(), null, "/index.xhtml?faces-redirect=true");
+
+        }
+    }
+}

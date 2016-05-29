f21bbe1558692a705ff7176f6cf2b8936ff0994f
FSSM-JAVAEE <elmezouari.asmae@gmail.com>
2016-05-29 23:35:56 +0100

    Creation de la page Budget et ajout du css de cette page

--- src/main/java/Beans/ButtonView.java
+++ src/main/java/Beans/ButtonView.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ ce java bean pour les messages declanche par la page budget
  */
 package beans;
 import javax.faces.application.FacesMessage;

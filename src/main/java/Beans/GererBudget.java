0c9649e600b598af51e2f5efa471521a0ae87db3
FSSM-JAVAEE <elmezouari.asmae@gmail.com>
2016-06-06 00:08:42 +0000

    Création de la classe de pérsistence DAO et Du ManagedBean

--- src/main/java/Beans/GererBudget.java
+++ src/main/java/Beans/GererBudget.java
@@ -1,57 +1,28 @@
 
 package Beans;
 
+import DAO.DAOAnneeBudgetaire;
+import controlers.ConnexionHibernateUtil;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import models.Anneebudgetaire;
+import org.hibernate.SessionFactory;
 @ApplicationScoped
 @ManagedBean
 public class GererBudget {
-    private int annee;
-    private double montant_RAP;
-    private double reliquat_RAP;
-
+    private Anneebudgetaire ab;
     public GererBudget() {
     }
-
-    public int getAnnee() {
-        return annee;
+    public Anneebudgetaire ajouter(){
+    DAOAnneeBudgetaire DAOab=new DAOAnneeBudgetaire(ConnexionHibernateUtil.getSessionFactory());
+    return DAOab.add(ab);
     }
 
-    public void setAnnee(int annee) {
-        this.annee = annee;
+    public Anneebudgetaire getAb() {
+        return ab;
     }
 
-    public double getMontant_RAP() {
-        return montant_RAP;
+    public void setAb(Anneebudgetaire ab) {
+        this.ab = ab;
     }
-
-    public void setMontant_RAP(double montant_RAP) {
-        this.montant_RAP = montant_RAP;
-    }
-
-    public double getReliquat_RAP() {
-        return reliquat_RAP;
-    }
-
-    public void setReliquat_RAP(double reliquat_RAP) {
-        this.reliquat_RAP = reliquat_RAP;
-    }
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final GererBudget other = (GererBudget) obj;
-        if (this.annee != other.annee) {
-            return false;
-        }
-        return true;
-    }
-     
 }
